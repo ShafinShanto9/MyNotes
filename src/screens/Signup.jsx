ea655c319af22dbae4e89fd648da0da541a2722e
shafin_shant0 <shafinshanto9@gmail.com>
2022-06-05 13:33:14 +0600

    radio container design complete

--- src/screens/Signup.jsx
+++ src/screens/Signup.jsx
@@ -4,7 +4,8 @@ import { Pressable } from 'react-native/';
 import Button from '../components/Button';
 import InputField from '../components/InputField';
 
-export default function Signup({navigation}) {
+export default function Signup({ navigation }) {
+  const selected = true
   return (
     <SafeAreaView style={{marginTop:Platform.OS === 'android' ? StatusBar.currentHeight:0, padding:10, flex:1}}>
       
@@ -16,9 +17,17 @@ export default function Signup({navigation}) {
         <InputField placeholder='Enter Your Age' />
         
         <Pressable style={styles.radioContainer}>
+          <View style={[styles.outterCircle, selected && styles.selectedOuterCircle ]}>
+            <View style={[styles.innerCircle, selected && styles.selectedInnerCircle ]}></View>
+          </View>
+          <Text style={styles.radioText}>Male</Text>
+        </Pressable>
+
+        <Pressable style={styles.radioContainer}>
           <View style={styles.outterCircle}>
             <View style={styles.innerCircle}></View>
           </View>
+          <Text style={styles.radioText}>Female</Text>
         </Pressable>
 
         <Button title={"sign up"} customStyles={{alignSelf:'center', marginTop:30 }}/>
@@ -37,5 +46,38 @@ export default function Signup({navigation}) {
 }
 
 const styles = StyleSheet.create({
-  
+  radioContainer:{
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10
+  },
+  outterCircle: {
+    height: 30,
+    width: 30,
+    borderRadius: 15,
+    borderWidth: 1,
+    borderColor: '#cfcfcf',
+    marginRight: 10,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  innerCircle: {
+    height: 16,
+    width: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: '#cfcfcf',
+    
+  },
+  radioText: {
+
+  },
+  selectedOuterCircle: {
+    borderColor: 'purple'
+  },
+  selectedInnerCircle: {
+    backgroundColor: 'purple',
+    borderColor:'purple'
+  }
+
 })
